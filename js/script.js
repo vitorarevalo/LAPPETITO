4846af5acc3cd230c3d8391a5f99e6cc0018baec
Vitor Alexandre Osuna Arevalo <vitinhoosuna2008@gmail.com>
2026-03-29 01:41:59 -0300

    feat: adicionei o carrossel dos pratos
    adição dos pratos da primeira seção

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// var IMG1 = document.querySelector("#slide1")
+// var IMG2 = document.querySelector("#slide2")
+// var IMG3 = document.querySelector("#slide3")
+// var IMG4 = document.querySelector("#slide4")
+
+// var slider = document.querySelector(".slider")
+
+// var sliderBtn1 = document.querySelector("#slideBtn1")
+// var sliderBtn2 = document.querySelector("#slideBtn2")
+// var sliderBtn3 = document.querySelector("#slideBtn3")
+// var sliderBtn4 = document.querySelector("#slideBtn4")
+
+
+// sliderBtn1.addEventListener("click", function() {
+//     slider.style.transform = "translateX(1000%)"
+// })
