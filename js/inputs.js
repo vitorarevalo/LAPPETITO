425a046493ac05060a6959439145b666162afb01
Vitor Alexandre Osuna Arevalo <vitinhoosuna2008@gmail.com>
2026-05-10 21:45:54 -0300

    feat: confirmação de envio de formulário adicionada

--- js/inputs.js
+++ js/inputs.js
@@ -19,3 +19,32 @@ const hoje = new Date();
 const hojeFormatado = hoje.toISOString().split('T')[0];
 
 dateInput.setAttribute('min', hojeFormatado);
+
+
+
+const form = document.querySelector('.reservation-form');
+const confirmDiv = document.querySelector('.confirm');
+const progressBarStat = document.querySelector('.progress-bar-stat');
+
+let timerRedirecionamento;
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    confirmDiv.style.display = 'flex';
+
+    progressBarStat.classList.add('animate-progress');
+
+    timerRedirecionamento = setTimeout(() => {
+        window.location.href = "index.html";
+    }, 5000);
+});
+
+document.querySelector('.close-btn').addEventListener('click', () => {
+    clearTimeout(timerRedirecionamento)
+    confirmDiv.style.display = 'none';
+    progressBarStat.classList.remove('animate-progress');
+
+    location.reload()
+});
