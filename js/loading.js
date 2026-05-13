5f66f6a7ec340e042bc1b2ccbe78a62d9d8b22ae
Vitor Alexandre Osuna Arevalo <vitinhoosuna2008@gmail.com>
2026-05-12 23:32:15 -0300

    style: versão para desktop concluída

--- js/loading.js
+++ js/loading.js
@@ -2,11 +2,49 @@ if (history.scrollRestoration) {
     history.scrollRestoration = 'manual';
 }
 
+var body = document.querySelector("body")
+
 var loadingScreen = document.querySelector(".loading-screen")
+var mainContent = document.querySelector(".main-content");
 
 document.body.style.overflowY = 'hidden';
 
 setTimeout(() => {
     loadingScreen.style.display = 'none'
     document.body.style.overflowY = 'auto';
+
+    
+    gsap.fromTo(mainContent, {
+        opacity: 0,
+    },
+    
+    { 
+        opacity: 1,
+        duration: 1.2,
+        ease: "power3.out"
+    }
+    
+);
+
+
+gsap.from(".slider", {
+    y: 1000,
+    duration: 1,
+    opacity: 0
+})
+
+gsap.from("#special-text", {
+    x: 1000,
+    duration: 1,
+    opacity: 0
+})
+
+gsap.from(".border", {
+    x: -400,
+    duration: 1,
+    opacity: 0
+})
+
+
+
 }, 5100)
